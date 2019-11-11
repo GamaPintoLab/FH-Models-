e618fb8aee8aac893125b7707af33fbec6632dd9
Marta-Correia <44930170+Marta-Correia@users.noreply.github.com>
2019-11-11 13:27:12 +0000

    Add files via upload

---
+++ Top10 models for new data.R
@@ -0,0 +1,48 @@
+## Title: Top10 models for new data
+## Author: Marta Correia
+## Date: 11.11.2019
+
+##--------------------------------------------------------------------------------##
+
+## Set directory - the same folder to which rda files were downloaded 
+
+setwd("C:/Users/MARTA/Desktop/Top10_models") #example
+
+
+## Load top10 models
+
+Sel1 <- readRDS("Sel1.rda")
+Sel3 <- readRDS("Sel3.rda")
+RFEct_A <- readRDS("RFEct_A.rda")
+RFEct_BL <- readRDS("RFEct_BL.rda")
+RFE78t_B <- readRDS("RFE78t_B.rda")
+RFE78t_Ad <- readRDS("RFE78t_Ad.rda")
+RFE78ct_Ad <- readRDS("RFE78ct_Ad.rda")
+RFE78ct_BL <- readRDS("RFE78ct_BL.rda")
+Imp_B <- readRDS("Imp_B.rda")
+Imp_AdL <- readRDS("Imp_AdL.rda")
+
+
+## Import new data
+
+library(readxl)
+newdata <- read_excel("newdata.xlsx") #example with excel file
+
+str(newdata) #check if all parameters are considered numeric and if 'newdata' is 
+             #considered a table or a data.frame 
+
+attach(newdata) #put parameters of newdata available for predictions
+
+
+## Make predictions for new data
+
+Pred <- predict(Sel1, newdata = "newdata") #newdata = "name of the data.frame with new data"
+
+Pred_class <- cbind(newdata, Pred) #add predictions as an extra column in data table
+
+
+#Use similar code for predictions using the other models
+
+
+## NOTE: FH-positive and FH-negative classes are codified as '1' and '2', respectively 
+
